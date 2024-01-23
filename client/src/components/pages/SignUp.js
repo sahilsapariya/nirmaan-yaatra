9a46e6eedc23d59b77da4e4b97332a4df65e54ef
Bhuvnesh Sanathara <68009290+bhuvisanathra@users.noreply.github.com>
2024-01-23 21:48:07 +0530

    Added Signin
    Added Sign in page and with signin page added routes  from which on success API call we can move from one signup page to signin page

--- client/src/components/pages/SignUp.js
+++ client/src/components/pages/SignUp.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import "../styles/SignUp.css"
 import buldingImage from '../../images/img_construction.png'
 
@@ -9,10 +9,8 @@ const options = [
 ];
 
 
-
-
 function SignUp() {
-
+    const navigate = useNavigate();
     const [formData, setFormData] = useState({
         username: '',
         email: '',
@@ -44,6 +42,7 @@ function SignUp() {
             } else {
                 console.log(formData)
                 console.error('API call failed');
+                navigate("/sign-in")
             }
         } catch (error) {
             console.error('Error during API call', error);
@@ -57,50 +56,56 @@ function SignUp() {
                     <div className='signup__building_image'>
                         <img src={buldingImage} alt='Background Image' />
                     </div>
-                    <div className='signup__building_input_form'>
-                        <h1 className="signup__container_heading">Sign Up</h1>
-                        <form onSubmit={handleSubmit}>
-                            <input
-                                type='text'
-                                name='username'
-                                value={formData.username}
-                                onChange={handleChange}
-                                className='signup__container_input_field'
-                                placeholder='Username'
-                                required
-                            />
-                            <input
-                                type='email'
-                                name='email'
-                                value={formData.email}
-                                onChange={handleChange}
-                                className='signup__container_input_field'
-                                placeholder='Email'
-                                required
-                            />
-                            <input
-                                type='password'
-                                name='password'
-                                value={formData.password}
-                                onChange={handleChange}
-                                className='signup__container_input_field'
-                                placeholder='Password'
-                                required
-                            />
-                            <select
-                                name='role'
-                                value={formData.role}
-                                onChange={handleChange}
-                            >
-                                <option value="" disabled>Role</option>
-                                {options.map((option) => (
-                                    <option key={option.value} value={option.value}>
-                                        {option.label}
-                                    </option>
-                                ))}
-                            </select>
-                            <button className='button_property' type='submit'>Submit</button>
-                        </form>
+                    <div className='signup__form_division'>
+                        <div className='logo_header'>
+                            <i class="fa-solid fa-user"></i>
+                            <h1>Nirmaanyatra</h1>
+                        </div>
+                        <div className='signup__building_input_form'>
+                            <h1 className="signup__container_heading">Sign Up</h1>
+                            <form onSubmit={handleSubmit}>
+                                <input
+                                    type='text'
+                                    name='username'
+                                    value={formData.username}
+                                    onChange={handleChange}
+                                    className='signup__container_input_field'
+                                    placeholder='Username'
+                                    required
+                                />
+                                <input
+                                    type='email'
+                                    name='email'
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    className='signup__container_input_field'
+                                    placeholder='Email'
+                                    required
+                                />
+                                <input
+                                    type='password'
+                                    name='password'
+                                    value={formData.password}
+                                    onChange={handleChange}
+                                    className='signup__container_input_field'
+                                    placeholder='Password'
+                                    required
+                                />
+                                <select
+                                    name='role'
+                                    value={formData.role}
+                                    onChange={handleChange}
+                                >
+                                    <option value="" disabled>Role</option>
+                                    {options.map((option) => (
+                                        <option key={option.value} value={option.value}>
+                                            {option.label}
+                                        </option>
+                                    ))}
+                                </select>
+                                <button className='button_property' type='submit'>Submit</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
