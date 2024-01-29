05b0b55b133e449b6716385b10b39056402c0c81
Bhuvnesh Sanathara <68009290+bhuvisanathra@users.noreply.github.com>
2024-01-30 00:23:10 +0530

    Added Add Site Page

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,11 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignUp from "./components/pages/SignUp";
 import SignIn from "./components/pages/SignIn";
 import AdminHome from "./components/pages/AdminHome";
+import AddSite from "./components/pages/AddSite";
+
 
 import ProtectedRoute from "./context/ProtectedRoute";
 import { AuthProvider } from "./context/AuthContext";
 
 const ProjectRoutes = () => {
+<<<<<<< Updated upstream
   return (
     <Router>
       <AuthProvider>
@@ -28,13 +31,31 @@ const ProjectRoutes = () => {
                 <AdminHome />
               </ProtectedRoute>
             }
+=======
+return (
+<Router>
+  <AuthProvider>
+    <Routes>
+      <Route path="/sign-up" element={ <ProtectedRoute>
+        <SignUp />
+        </ProtectedRoute>
+        }
+        />
+        <Route path="/sign-in" exact element={<SignIn />} />
+        <Route path="/admin-home" element={ <ProtectedRoute>
+          <AdminHome />
+          </ProtectedRoute>
+          }
+>>>>>>> Stashed changes
           />
 
           <Route path="/*" element={<SignIn />} />
-        </Routes>
-      </AuthProvider>
-    </Router>
-  );
+
+          <Route path="/add-site" excat element={<AddSite />} />
+    </Routes>
+  </AuthProvider>
+</Router>
+);
 };
 
 export default ProjectRoutes;
