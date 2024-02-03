d309ad1682a224f3de05e5d9213149683f74cbc3
sahilsapariya <sahilsapariya03@gmail.com>
2024-02-03 18:22:02 +0530

    added redux-persist to the project

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,12 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
-import projectReducer from "../features/projects/projectSlice";
-import globalReducer from "../features/global/globalSlice"
-import siteReducer from "../features/site/siteSlice"
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import projectReducer from '../features/projects/projectSlice';
+import globalReducer from '../features/global/globalSlice';
+import siteReducer from '../features/site/siteSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  project: projectReducer,
+  global: globalReducer,
+  site: siteReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    project: projectReducer,
-    global: globalReducer,
-    site: siteReducer
-  },
+  reducer: persistedReducer,
 });
+
+export const persistor = persistStore(store);
