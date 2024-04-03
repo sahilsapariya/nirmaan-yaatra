637c47032973e667473e50bd69d3cd127ce5166e
sahilsapariya <sahilsapariya03@gmail.com>
2024-04-04 04:11:52 +0530

    database changed to sqlite

--- server/server/settings.py
+++ server/server/settings.py
@@ -109,23 +109,23 @@ WSGI_APPLICATION = "server.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "nirmaan_yaatra",
-        "USER": "postgres",
-        "PASSWORD": "postgresql",
-        "HOST": "localhost",
-        "PORT": "5432"
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "nirmaan_yaatra",
+#         "USER": "postgres",
+#         "PASSWORD": "postgresql",
+#         "HOST": "localhost",
+#         "PORT": "5432"
+#     }
+# }
 
 
 # Password validation
